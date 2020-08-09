9944fa2006d8c14e9d946dd66622e0a1850c91c7
Sam <barrett370@gmail.com>
2020-08-09 21:09:10 +0000

    change to postgresql db in docker compose

--- bridgesite/dev_settings.py
+++ bridgesite/dev_settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'bridgesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'django',
+        'PASSWORD': 'pg_pass',
+        'HOST': 'db',
+        'PORT': '5432',
+
     }
 }
 
