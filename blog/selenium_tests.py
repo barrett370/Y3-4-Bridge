51919b94b2e217a8f26012421159a386fdee1233
Sam <barrett370@gmail.com>
2020-08-21 22:09:03 +0000

    EOD

--- blog/selenium_tests.py
+++ blog/selenium_tests.py
@@ -1,7 +1,7 @@
 ## Selenium Tests
 
 from django.contrib.auth.models import User
-from blog.models import Post
+from blog.models import *
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -16,6 +16,36 @@ class BlogFunctionalTest(StaticLiveServerTestCase):
     test_text = "test text"
     test_create_date = datetime.fromtimestamp(1326244364, tz=pytz.UTC)
     test_publish_date = datetime.fromtimestamp(1326244375, tz=pytz.UTC)
+
+    test_skill_id = "0e538346-0474-4ee0-8f7f-c47f2568b6ee"
+    test_skill_title = "Test Skill"
+    test_skill_desc = "This is a test skill for functional tests"
+    test_skill_exp = "1s"
+
+    test_uni_course_id = "dbe21966-5b8a-442c-8f70-0f840fd12ae5"
+    test_uni_course_title = "Test University Course"
+    test_uni_course_description = (
+        "This is a test course for functional testing purposes"
+    )
+    test_uni_course_completion_date = datetime.fromtimestamp(
+        1326244375, tz=pytz.UTC
+    ).date()
+    test_uni_course_grade = "70%"
+    test_uni_course_level = "university"
+
+    test_misc_course_id = "89a6e329-4d21-41fe-9688-a3f259bd9dce"
+    test_misc_course_title = "Test Misc Course"
+    test_misc_course_description = (
+        "This is a test course for functional testing purposes"
+    )
+    test_misc_course_completion_date = datetime.fromtimestamp(
+        1326244375, tz=pytz.UTC
+    ).date()
+    test_misc_course_grade = "70%"
+    test_misc_course_level = "other"
+
+    test_cv_phone_number = "12345678901"
+    test_cv_email_address = "foo@bar.baz"
 
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -56,3 +86,63 @@ class BlogFunctionalTest(StaticLiveServerTestCase):
         self.assertEqual(post_title.text, self.test_title)
         post_text = self.driver.find_element_by_id("post_text")
         self.assertEqual(post_text.text, self.test_text)
+
+    def create_test_cv(self):
+
+        # create test tech skill
+        TechSkill.objects.create(
+            skill_id=self.test_skill_id,
+            title=self.test_skill_title,
+            description=self.test_skill_desc,
+            experience_amount=self.test_skill_exp,
+        )
+
+        # Create a test univeristy module
+        Course.objects.create(
+            course_id=self.test_uni_course_id,
+            title=self.test_uni_course_title,
+            description=self.test_uni_course_description,
+            completion_date=self.test_uni_course_completion_date,
+            grade=self.test_uni_course_grade,
+            course_level=self.test_uni_course_level,
+        )
+
+        # Create a test misc module
+        Course.objects.create(
+            course_id=self.test_misc_course_id,
+            title=self.test_misc_course_title,
+            description=self.test_misc_course_description,
+            completion_date=self.test_misc_course_completion_date,
+            grade=self.test_misc_course_grade,
+            course_level=self.test_misc_course_level,
+        )
+
+        # Create test cv
+        CV.objects.create(
+            phone_number=self.test_cv_phone_number,
+            email_address=self.test_cv_email_address,
+        )
+
+    def test_cv(self):
+        self.create_test_cv()
+        self.driver.get(self.live_server_url + "/cv")
+        expected_cv = open("expected_pages/expected_cv.html", "r").read()
+
+        # f = open("exception_cv.html", "w")
+        # f.write(self.driver.page_source)
+        # f.close()
+        self.assertEqual(self.driver.page_source, expected_cv)
+
+    def test_navigation_bar(self):
+        self.create_test_cv()
+        body = self.driver.find_element_by_tag_name("body")
+        print(body.text)
+        nav_bar = body.find_element_by_id("nav-bar")
+        nav_bar.find_element_by_link_text("Home").click()
+
+        expected_cv = open("expected_pages/expected_cv.html", "r").read()
+
+        self.assertEqual(self.driver.page_source, expected_cv)
+        self.create_test_post()
+        self.driver.find_element_by_link_text("My CV").click()
+
